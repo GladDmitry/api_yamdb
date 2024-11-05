7e3b5461796e6ae200103f0bc7fbd912d55e88c9
Miroslav Nikolaichenko <mirnik7thebest@yandex.com>
2024-11-06 00:04:53 +0300

    csv

--- api_yamdb/reviews/management/commands/loadcsv.py
+++ api_yamdb/reviews/management/commands/loadcsv.py
@@ -1,121 +1,57 @@
-from csv import DictReader
+import csv
 
-from django.core.management.base import BaseCommand
-
-from api_yamdb.settings import BASE_DIR
-from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+from django.core.management.base import BaseCommand, CommandError
+from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import CustomUser
 
 
+CSV_PATH = 'static/data/'
+
+FOREIGN_KEY_FIELDS = ('category', 'author')
+
+DICT = {
+    CustomUser: 'users.csv',
+    Genre: 'genre.csv',
+    Category: 'category.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
 class Command(BaseCommand):
-    help = 'Импорт данных из csv файлов'
+    help = 'Загрузка данных из файла формата csv в базу данных.'
 
-    def ImportUser(self):
-        if CustomUser.objects.exists():
-            print('Данные для User уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/users.csv',
-                    encoding='utf8')):
-                CustomUser.objects.create(
-                    id=row['id'],
-                    username=row['username'],
-                    email=row['email'],
-                    role=row['role'],
-                    bio=row['bio'],
-                    first_name=row['first_name'],
-                    last_name=row['last_name'],)
-            print('Данные для User загружены')
+    def csv_serializer(self, csv_data, model):
+        objs = []
+        for row in csv_data:
+            for field in FOREIGN_KEY_FIELDS:
+                if field in row:
+                    row[f'{field}_id'] = row[field]
+                    del row[field]
+            objs.append(model(**row))
+        model.objects.bulk_create(objs)
 
-    def ImportGenre(self):
-        if Genre.objects.exists():
-            print('Данные для Genre уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/genre.csv',
-                    encoding='utf8')):
-                Genre.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],)
-            print('Данные для genre загружены')
-
-    def ImportCategory(self):
-        if Category.objects.exists():
-            print('Данные для Category уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/category.csv',
-                    encoding='utf8')):
-                Category.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],)
-            print('Данные для Category загружены')
-
-    def ImportTitle(self):
-        if Title.objects.exists():
-            print('Данные для Title уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/titles.csv',
-                    encoding='utf8')):
-                Title.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    year=row['year'],
-                    category=Category.objects.get(id=row['category']),)
-            print('Данные для Title загружены')
-
-    def ImportGenreTitle(self):
-        if GenreTitle.objects.exists():
-            print('Данные для GenreTitle уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/genre_title.csv',
-                    encoding='utf8')):
-                GenreTitle.objects.create(
-                    id=row['id'],
-                    title_id=row['title_id'],
-                    genre_id=row['genre_id'],)
-            print('Данные для GenreTitle загружены')
-
-    def ImportReview(self):
-        if Review.objects.exists():
-            print('Данные для Review уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/review.csv',
-                    encoding='utf8')):
-                Review.objects.create(
-                    id=row['id'],
-                    title_id=row['title_id'],
-                    text=row['text'],
-                    author=CustomUser.objects.get(id=row['author']),
-                    score=row['score'],
-                    pub_date=row['pub_date'],)
-            print('Данные для Review загружены')
-
-    def ImportComment(self):
-        if Comment.objects.exists():
-            print('Данные для Comment уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/comments.csv',
-                    encoding='utf8')):
-                Comment.objects.create(
-                    id=row['id'],
-                    review_id=row['review_id'],
-                    text=row['text'],
-                    author=CustomUser.objects.get(id=row['author']),
-                    pub_date=row['pub_date'],)
-            print('Данные для Comment загружены')
+    def load_csv(self, model):
+        try:
+            with open(
+                CSV_PATH + DICT[model],
+                newline='',
+                encoding='utf8'
+            ) as csv_file:
+                self.csv_serializer(csv.DictReader(csv_file), model)
+            self.stdout.write(self.style.SUCCESS(
+                f'Файл {model} успешно загружен.'))
+        except Exception as error:
+            CommandError(error)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Данные из файла {DICT[model]} успешно занесены в БД"
+            )
+        )
 
     def handle(self, *args, **kwargs):
-        self.ImportUser()
-        self.ImportGenre()
-        self.ImportCategory()
-        self.ImportTitle()
-        self.ImportGenreTitle()
-        self.ImportReview()
-        self.ImportComment()
+        for model in DICT:
+            self.load_csv(model)
+        self.stdout.write(self.style.SUCCESS(
+            'Все файлы успешно загружены в базу данных.'))
